97a04f5182ddc1254280ea6ab6dbfffde6b522bf
Diana <diana.castillo7676@alumnos.udg.mx>
2023-05-31 22:20:04 -0600

    Actualización de assets

--- js/player.js
+++ js/player.js
@@ -5,13 +5,13 @@ export default class Player {
     const anims = scene.anims;
     anims.create({
       key: "player-walk",
-      frames: anims.generateFrameNumbers("characters", { start: 46, end: 49 }),
+      frames: anims.generateFrameNumbers("characters", { start: 23, end: 26 }),
       frameRate: 8,
       repeat: -1,
     });
     anims.create({
       key: "player-walk-back",
-      frames: anims.generateFrameNumbers("characters", { start: 65, end: 68 }),
+      frames: anims.generateFrameNumbers("characters", { start: 42, end: 45 }),
       frameRate: 8,
       repeat: -1,
     });
@@ -64,8 +64,8 @@ export default class Player {
       sprite.anims.stop();
 
       // If we were moving, pick and idle frame to use
-      if (prevVelocity.y < 0) sprite.setTexture("characters", 65);
-      else sprite.setTexture("characters", 46);
+      if (prevVelocity.y < 0) sprite.setTexture("characters", 42);
+      else sprite.setTexture("characters", 23);
     }
   }
 
