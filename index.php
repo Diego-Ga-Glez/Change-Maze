d57e3cbbcac8d4a0f09f172e5a72096cde8fafa8
Diana <diana.castillo7676@alumnos.udg.mx>
2023-10-29 17:24:51 -0600

    Aviso de privacidad e ícono

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ChangeMaze</title>
+    <link rel="icon" href="ChangeMaze/assets/spritesheets/icono.png" type="image">
 
     <!-- Agregar todos las bibliotecas necesitadas -->
 
